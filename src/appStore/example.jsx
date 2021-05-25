3318d964c5d8156eb48ce8a29931a165a4e66b3c
Rudra Roy <43406144+astrarudra@users.noreply.github.com>
2021-05-25 22:14:28 +0530

    R2: Renamed Components

--- src/appStore/example.jsx
+++ src/appStore/example.jsx
@@ -7,7 +7,7 @@ var example = {
         {id:2, data: "example2"},
     ],
     justList: ['abc', 123],
-    email: "astrarudra8@gmail.com"
+    email: "rudraroy1507@gmail.com"
 }
 
 // Store Actions - These should be pure functions.
@@ -19,19 +19,16 @@ const slice = createSlice({
         changeEmail: (example, action) => {
             var { email } = action.payload
             example.email = email
-            return example
         },
         changeData: (example, action) => {
             var { id, data } = action.payload
             example.objList.forEach(o => {
                 if(o.id === id) o.data = data
             })
-            return example
         },
         addList: (example, action) => {
             var { justList } = example
             justList.push("Dummy")
-            return example
         },
     }
 })
